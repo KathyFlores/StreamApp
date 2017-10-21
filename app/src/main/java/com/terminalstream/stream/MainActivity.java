e1a5300938043b805988b67f11fb846ab4f2da42
kathyf <wmiao018@163.com>
2017-10-21 22:34:41 +0800

    first commit

---
+++ app/src/main/java/com/terminalstream/stream/MainActivity.java
@@ -0,0 +1,17 @@
+package com.terminalstream.stream;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
