fe51aad4ea951af3bebb4a2db21f6208ab6df5a5
Alan <Alan@AlanSwift-3.local>
2017-12-07 09:09:57 +0800

    fix userid, fix service, add TODO

--- app/src/main/java/stream/com/streamapp/constant/regex.java
+++ app/src/main/java/stream/com/streamapp/constant/regex.java
@@ -7,4 +7,6 @@ package stream.com.streamapp.constant;
 public class regex {
     public static final String passwdPattern="passwd<br>.*?<br>";
     public static final String phonePattern="^[1][3,4,5,8][0-9]{9}$";
+    //<br>id<br>1<br>
+    public static final String idPattern="<br>id<br>.*?<br>";
 }
