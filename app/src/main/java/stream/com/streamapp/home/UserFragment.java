b1657073c5b49cd671fd57dcd683167d6b6049da
kathyf <wmiao018@163.com>
2017-12-20 22:00:09 +0800

    添加帮助与反馈、关于我们的对话框显示，上传报告初版

--- app/src/main/java/stream/com/streamapp/home/UserFragment.java
+++ app/src/main/java/stream/com/streamapp/home/UserFragment.java
@@ -1,11 +1,14 @@
 package stream.com.streamapp.home;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,6 +39,7 @@ public class UserFragment extends Fragment {
     private View view;
     private Button logoutBTN;
     private Drawable avatarSrc;
+    private AlertDialog.Builder builder;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_user, null);
         initView();
@@ -62,6 +66,16 @@ public class UserFragment extends Fragment {
         backup=view.findViewById(R.id.backup);
         about=view.findViewById(R.id.about);
         help=view.findViewById(R.id.help);
+        builder =new AlertDialog.Builder(getContext());
+        builder.setNegativeButton("取消",null);
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(true);
 
     }
     private void setListener(){
@@ -87,6 +101,22 @@ public class UserFragment extends Fragment {
                 i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(i);
 
+            }
+        });
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.setTitle(R.string.help);
+                builder.setMessage("如果您在使用过程中遇到问题，请将您的问题与详细描述发至wmiao018@163.com，我们将尽全力为您解决并给您一个满意的答复！");
+                builder.show();
+            }
+        });
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.setTitle(R.string.about);
+                builder.setMessage("流水是浙江大学智能终端2017年秋学期课程的一个课程作业，是一款条目清晰、可自动同步电子支付账户、支持导出账单、方便快捷且安全的Android记账APP。\n开发者：沈锴，吴亦全，王淼");
+                builder.show();
             }
         });
     }
